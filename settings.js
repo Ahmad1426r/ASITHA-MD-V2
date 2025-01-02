1d31af1070976593d1dc25c62bc102e70ee68d0c
Ahmad1426r <riaz1426rr@gmail.com>
2025-01-02 14:49:43 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '' : 𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=nrhlTAZJ#ycoKck2MuuIzld9klDanVR3N0tY3X86hq_iimnpsps0,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
